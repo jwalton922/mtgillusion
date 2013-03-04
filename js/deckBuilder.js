0e029236c218b60eac52e58afb6f1a19e14b0994
Josh Walton <jwalton922@gmail.com>
2013-03-03 18:44:27 -0700

    serve static files?

--- js/deckBuilder.js
+++ js/deckBuilder.js
@@ -7,11 +7,16 @@ function DeckBuilder($scope, $http, $log) {
         var url = "http://gatherer.wizards.com/Handlers/InlineCardSearch.ashx?nameFragment=a";
         var params = {};
         params.nameFragment = $scope.searchText;
+        params["JSON_CALLBACK"] = "$scope.processSearchResults";
 
-        $http.get(url, {params: params}).success(function(xhr) {
+        $http.jsonp(url, {params: params}).success(function(xhr) {
             $log.log("Search results: "+angular.toJson(xhr));
         }
 
         );
+    }
+    
+    $scope.processSearchResults = function(data){
+        $log.log("processing search results: "+angular.toJson(data));
     }
 }
